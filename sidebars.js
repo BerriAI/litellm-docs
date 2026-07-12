8da6e2d433608d285c01eb6fccace629809f38b0
amdmsz <69088549+amdmsz@users.noreply.github.com>
2026-07-12 17:07:54 +0800

    Register byesu provider in sidebar

--- sidebars.js
+++ sidebars.js
@@ -1050,6 +1050,7 @@ const sidebars = {
         "providers/baseten",
         "providers/black_forest_labs",
         "providers/black_forest_labs_img_edit",
+        "providers/byesu",
         "providers/bytez",
         "providers/cerebras",
         "providers/chutes",
