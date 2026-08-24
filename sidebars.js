6b73591023a15f4e55b7d483200e73be0ea4be49
Artem Burei <artemburej5@gmail.com>
2026-08-24 19:06:50 +0200

    docs: add LLM Tech (llmtech) provider page

--- sidebars.js
+++ sidebars.js
@@ -1156,6 +1156,7 @@ const sidebars = {
         "providers/lemonade",
         "providers/llamafile",
         "providers/llamagate",
+        "providers/llmtech",
         "providers/lm_studio",
         "providers/manus",
         "providers/meta",
